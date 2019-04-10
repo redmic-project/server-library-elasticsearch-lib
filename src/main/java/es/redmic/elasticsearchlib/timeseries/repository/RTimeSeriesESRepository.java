d8a27bbe48f615bd44f4b969b747586632498e67
Noel Alonso <noel@redmic.es>
2019-04-10 10:36:08 +0100

    Añade termquery común para todas las timeseries

--- src/main/java/es/redmic/elasticsearchlib/timeseries/repository/RTimeSeriesESRepository.java
+++ src/main/java/es/redmic/elasticsearchlib/timeseries/repository/RTimeSeriesESRepository.java
@@ -1,5 +1,11 @@
 package es.redmic.elasticsearchlib.timeseries.repository;
 
+import java.util.List;
+import java.util.Map;
+
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.joda.time.format.DateTimeFormat;
 
 import es.redmic.elasticsearchlib.data.repository.RDataESRepository;
@@ -16,5 +22,21 @@ public abstract class RTimeSeriesESRepository<TModel extends BaseTimeDataAbstrac
 	@Override
 	protected String getIndex(final TModel modelToIndex) {
 		return getIndex()[0] + "-" + modelToIndex.getDate().toString(DateTimeFormat.forPattern("yyyy-MM-dd"));
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public QueryBuilder getTermQuery(Map<String, Object> terms, BoolQueryBuilder query) {
+
+		if (terms.containsKey("dataDefinition")) {
+			List<Integer> ids = (List<Integer>) terms.get("dataDefinition");
+			query.must(QueryBuilders.boolQuery().filter(QueryBuilders.termsQuery("dataDefinition", ids)));
+		}
+
+		if (terms.containsKey("dates")) {
+			List<String> dates = (List<String>) terms.get("dates");
+			query.must(QueryBuilders.boolQuery().filter(QueryBuilders.termsQuery("date", dates)));
+		}
+		return super.getTermQuery(terms, query);
 	}
 }
